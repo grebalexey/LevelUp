0fb3b5309c83c25ab3bd528b3936e458676fcba8
grebalexey <136621926+grebalexey@users.noreply.github.com>
2024-07-17 02:02:30 +0300

    вроде все

--- script/script.js
+++ script/script.js
@@ -46,4 +46,25 @@ document.addEventListener("DOMContentLoaded", () => {
         countdown.innerHTML = `${minutes}:${seconds}`;
         time --;
     }
+
+    // ввод только цифр в input
+
+    const input = document.getElementById('phone');
+
+    input.addEventListener('keydown', function(event) {
+    
+    if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+        
+        (event.keyCode == 65 && event.ctrlKey === true) ||
+        
+        (event.keyCode >= 35 && event.keyCode <= 39)) {
+    
+    return;
+    } else {
+        
+        if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+        event.preventDefault();
+    }
+    }
+    });
 })
